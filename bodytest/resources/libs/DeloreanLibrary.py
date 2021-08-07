a5a57acf359b24982b04cae69a688a3bac6d5865
Mathias, George <gmathias80@gmail.com>
2021-08-06 23:47:43 -0300

    criando cenarios independentes

--- bodytest/resources/libs/DeloreanLibrary.py
+++ bodytest/resources/libs/DeloreanLibrary.py
@@ -2,10 +2,33 @@ import psycopg2
 from logging import info
 
 class DeloreanLibrary():
-    
+        
     def remove_student(self, email):
 
         query = "delete from students where email = '{}'".format(email)
+        info(query)
+
+        conn = psycopg2.connect(
+            host='ec2-107-20-153-39.compute-1.amazonaws.com',
+            database='dftfo325togtas',
+            user='xriezdxlokyzuw',
+            password='4241ef829cdf08b1a7d9c783287a7b670c730ca7f7c89959baeea6200010e829'
+        )
+
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+        conn.close()
+
+    
+    def insert_student(self, student):
+
+        self.remove_student(student['email'])
+
+        query = ("insert into students (name, email, age, weight, feet_tall, created_at, updated_at)"
+                 "values('{}', '{}', {}, {}, {}, now(), now());"
+                 .format(student['name'], student['email'], student['age'], student['weight'], student['feet_tall'])
+                 )
         info(query)
 
         conn = psycopg2.connect(
--- bodytest/tests/cadastro_alunos.robot
+++ bodytest/tests/cadastro_alunos.robot
@@ -17,3 +17,17 @@ Novo aluno
     Toaster Text Should Be    Aluno cadastrado com sucesso.
 
     [Teardown]                Thinking And Take Screenshot    1
+
+Não deve permitir email duplicado
+    [Tags]    dup
+
+    &{student}    Create Dictionary    name=Nadaly Farache    email=nadaly@gmail.com    age=37    weight=45    feet_tall=1.45
+
+    Insert Student            ${student}
+
+    Go To Students
+    Go To Form Student
+    New Student               ${student}
+    Toaster Text Should Be    Email já existe no sistema.
+
+    [Teardown]                Thinking And Take Screenshot    1
